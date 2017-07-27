06cc04f5120f26dd244b82bec45e84bff8169f0c
MAshraf <ee.mashraf@gmail.com>
2017-07-27 17:28:51 +0300

    scroll to features

--- scripts/app.js
+++ scripts/app.js
@@ -4,4 +4,10 @@
   $("html").niceScroll();
   //Change Header height
   $("header").height($(window).height());
+  //Scroll to features
+  $("header .arrow i").click(function(){
+    $("html, body").animate({
+      scrollTop: $(".features").offset().top
+    }, 1000);
+  });
 })();
